1a975f70f49ffbd00e5ae651b34613cad5157598
David Bonila <davidbonillamedina@gmail.com>
2021-11-17 17:17:19 +0100

    add Opcio-1.3.sh

---
+++ Opcio-1.3.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+Opcio-1.3()
+{	
+	clear
+	tail +3 cars.csv | sort -t ';' -n -k7 > YYY
+	
+	wc -l cars.csv > xxx
+	read lines filename < xxx; i=3
+	
+	while [ $i -le $lines ]
+	do
+		for x in {1..9}
+		do
+			head -$i YYY | tail -1 | cut -d';' -f$x > n$x
+		done
+		
+		echo "Cotxe: `cat n1`"
+		echo "MPG: `cat n2`	Nº Cilindres: `cat n3`		Cilindrada: `cat n4`"
+		echo "Potència(CV): `cat n5`	Pes: `cat n6`	Acceleració (0 a 100): `cat n7`"
+		echo "Model de l'any: `cat n8`		Origen: `cat n9`"
+		echo ""
+		
+		let i+=1
+		
+		for x in {1..9}
+		do
+			rm n$x
+		done
+	done
+	
+	rm xxx
+	rm YYY
+	
+	read -rs
+}
+
+Opcio-1.3
+source Opcio-1.sh
